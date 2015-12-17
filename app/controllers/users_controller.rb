89bb3373ced75121ef89e058547c330c2e291bc4
Estefania <sqr0502@gmail.com>
2015-12-16 22:49:10 -0500

    handle post request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
-  def new
-    puts 'this was hit'
-    @user = User.new
-  end
 
+  def getUsers
+    render :json => User.all
+  end
+  
   def create
     @user = User.new(user_params)
+     if @user.save
+       render :json => @user
+     end
+  end
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :project_repo, :project_url)
   end
 end
