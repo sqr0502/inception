2f42c232868b774e75f23aacf28c1a1e40c6a7d4
Estefania <sqr0502@gmail.com>
2015-12-18 08:27:01 -0500

    add questionaire

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -8,10 +8,60 @@ app.controller('formController', ['$scope', '$http', function($scope, $http){
   console.log('this works');
 
   $scope.addUser = function(){
+
+    $scope.user.ratings = $scope.ratings;
+
     $http.post('/api/user', $scope.user).then(function(res){
       if(res.status == 200){
             console.log('sucessful');
       }
     })
   }
+
+  $scope.ratings = [{
+    name: "use best practices for OOP",
+    rating: 0
+  }, {
+    name: "modular development",
+    rating: 0,
+  }, {
+    name: "full stack workflow understanding",
+    rating: 0
+  }, {
+    name: "testing",
+    rating: 0
+  }, {
+    name: "database knowledge",
+    rating: 0
+  }, {
+    name: "debugging",
+    rating: 0
+  }, {
+    name: "problem solving skills",
+    rating: 0
+  }, {
+    name: "javascript",
+    rating: 0
+  }, {
+    name: "html",
+    rating: 0
+  }, {
+    name: "css",
+    rating: 0
+  }, {
+    name: "working on a team",
+    rating: 0
+  }, {
+    name: "self-motivation",
+    rating: 0
+  }, {
+    name: "communication skills",
+    rating: 0
+  }, {
+    name: "your own energy level",
+    rating: 0
+  }, {
+    name: "intelligence",
+    rating: 0
+  }]
 }]);
