19b72d6a769fd26f731d0c390e85b6c468ee3f54
Estefania <sqr0502@gmail.com>
2015-12-18 22:24:32 -0500

    add email template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
          rating = Rating.new({:name => rate["name"], :rating => rate["rating"], :user_id => @user.id})
          rating.save
        end
-       sendEmail
+       sendEmail(data)
      end
   end
 
@@ -26,16 +26,38 @@ class UsersController < ApplicationController
     params.require(:user).permit(:full_name, :email, :project_repo, :project_url, :ratings)
   end
 
-  def sendEmail
+  def sendEmail(data)
+
+    #add the following objects to the string html data["full_name"] remember to treat them like variables
+    #otherwise they won't appear on the string
+
+    html = "<div style='background-color: #114B5F; width: 350px; padding: 3%;'><img style='display: block; margin: 0 auto;' src='http://trimagency.com/wp-content/uploads/2015/04/0.png'/>
+            <table style='background-color:#FFFFFF;  text-align:left; margin-top: 2%;'><thead><br><tr><th style='text-align: center;
+            font-size: 18px;' colspan='2'; margin-top: 5px;>Apprentice Submission</th></tr></thead><tbody>" +
+            "<tr><th >Name:</th><td>#{data['full_name']}</td></tr>" +
+             "<tr><th style=''>Email:</th><td>#{data['email']}</td></tr>" +
+             "<tr><th style='text-align: center; font-size: 18px;' colspan='2'>Project Info</th></tr>" +
+             "<tr><th style=''>Repo:</th><td><a href='#{data['project_repo']}'>#{data['project_repo']}</a></td></tr>" +
+             "<tr><th style=''>URL:</th ><td><a href='#{data['project_url']}'>#{data['project_url']}</a></td></tr>" +
+             "<br><tr ><th style='text-align: center; font-size: 18px;' colspan='2'; >Development Areas Rated</th></tr>"
+
+    #create a loop that adds the ratings to the string html
+    data["ratings"].each do |rating|
+      html += "<tr ><th style='text-transform: uppercase;'>#{rating['name']}</th><td style='text-align: center; '>#{rating['rating']}</td></tr>"
+
+    end
+
+    html +=  "</tbody></table></div>"
+
     client = SendGrid::Client.new do |c|
       c.api_key = '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'
     end
 
     mail = SendGrid::Mail.new do |m|
        m.to = 'sqr0502@gmail.com'
-       m.from = 'taco@cat.limo'
-       m.subject = 'Hello world!'
-       m.text = 'I heard you like pineapple.'
+       m.from = 'automated'
+       m.subject = 'Apprentice Submission'
+       m.html = html
      end
 
      res = client.send(mail)
