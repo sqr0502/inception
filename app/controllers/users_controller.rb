5c7e334f45f536730c0649c9268c15724f47f142
Estefania <sqr0502@gmail.com>
2015-12-20 09:44:23 -0500

    update email test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     end
 
     mail = SendGrid::Mail.new do |m|
-       m.to = 'lleo5301@trimagency.com​'
+       m.to = 'lleo5301@gmail.com'
        m.from = 'Automated'
        m.bcc = 'sqr0502@gmail.com'
        m.subject = 'Apprentice Submission'
